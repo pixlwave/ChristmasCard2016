9bdfe15dece94f76e6e708a5d413b0bd1280b4fd
Doug <git@pixlwave.uk>
2016-12-17 11:33:21 +0000

    Remove the stars from the background. Update initial instructions.

--- ChristmasCard/SnowScene.swift
+++ ChristmasCard/SnowScene.swift
@@ -60,7 +60,7 @@ class SnowScene: SKScene {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     func letItSnow() {
